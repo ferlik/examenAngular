713f661921d5ac4c9574d04feea029f3470d67a7
ferlik <ferlik@hotmail.fr>
2020-07-02 17:11:42 +0200

    push 2

--- src/app/components/modif-ordinateur/modif-ordinateur.component.ts
+++ src/app/components/modif-ordinateur/modif-ordinateur.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { OrdinateurService } from 'src/app/services/ordinateur.service';
+import { ToastrService } from 'ngx-toastr';
+import { Ordinateur } from 'src/app/models/ordinateur';
 
 @Component({
   selector: 'app-modif-ordinateur',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModifOrdinateurComponent implements OnInit {
 
-  constructor() { }
+  ordi : Ordinateur;
+  isLoading:boolean;
+  marqueDispo = this.ordinateurService.marqueDispo;
+  typeDispo = this.ordinateurService.typeDispo;
+  categoriDispo = this.ordinateurService.categoDispo;
+
+  constructor(private activatedRoute: ActivatedRoute, private ordinateurService: OrdinateurService, private toastr:ToastrService,private router:Router) { }
 
   ngOnInit(): void {
+    this.isLoading = true;
+    this.ordinateurService.getOneOrdi(parseInt(this.activatedRoute.snapshot.paramMap.get('id'))).subscribe((data: Ordinateur) => {
+      this.ordi = data;
+      this.isLoading = false;
+    });
+  }
+
+  modifOrdi(): void {
+    this.isLoading = true;
+    this.ordinateurService.modifOrdi(this.ordi).subscribe(then => {
+      this.isLoading = false;
+      this.router.navigate(['/home']);
+      this.toastr.success("Le PC à bien été modifié !");
+    })
   }
 
 }
